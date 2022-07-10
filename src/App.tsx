d8b13e285c8779b5ef4957b333d02be8ef61a6a9
OwnKng <owen.king@outlook.com>
2022-07-10 13:44:47 +0100

    Initial effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import Sketch from "./Sketch"
+import Text from "./Text"
 
 const App = () => (
   <Canvas shadows>
     <OrbitControls />
     <ambientLight intensity={0.5} />
+    <pointLight position={[0, 0, -10]} />
     <Sketch />
   </Canvas>
 )
--- src/Sketch.tsx
+++ src/Sketch.tsx
@@ -5,35 +5,39 @@ import { InstancedFlow } from "three/examples/jsm/modifiers/CurveModifier"
 
 const TWO_PI = Math.PI * 2
 
-const numberOfPoints = 50
+const numberOfPoints = 20
 
-const radius = 5
+const radius = 30
 
-const circle = Array.from(
-  { length: numberOfPoints },
-  (_, i) =>
-    new THREE.Vector3(
-      Math.sin((i / numberOfPoints) * TWO_PI) * radius,
-      Math.cos((i / numberOfPoints) * TWO_PI) * radius,
-      0
+const circle = Array.from({ length: numberOfPoints }, (_, i) =>
+  new THREE.Vector3()
+    .setFromSphericalCoords(
+      1,
+      Math.PI / 2 + (Math.random() - 0.5),
+      (i / numberOfPoints) * TWO_PI
     )
+    .multiplyScalar(radius)
 )
 
-const box = new THREE.BoxBufferGeometry(0.1, 0.1, 0.5)
+const curve = new THREE.CatmullRomCurve3(circle, true, "centripetal")
+
+const box = new THREE.ConeBufferGeometry(1, 3, 5)
 const boxMaterial = new THREE.MeshPhongMaterial({ color: "orange" })
 
 const Sketch = () => {
   const ref = useRef(null!)
 
-  const curve = useMemo(
-    () => new THREE.CatmullRomCurve3(circle, true, "centripetal"),
-    []
+  const points = curve.getPoints(50)
+
+  const line = new THREE.LineLoop(
+    new THREE.BufferGeometry().setFromPoints(points),
+    new THREE.LineBasicMaterial()
   )
 
   const flow = useMemo(() => {
-    box.rotateX(Math.PI)
+    box.rotateZ(-Math.PI * 0.5)
 
-    const numberOfInstances = 100
+    const numberOfInstances = 10
     const flow = new InstancedFlow(numberOfInstances, 1, box, boxMaterial)
     flow.updateCurve(0, curve)
 
@@ -43,9 +47,16 @@ const Sketch = () => {
     }
 
     return flow
-  }, [curve])
+  }, [])
 
-  return <primitive castShadow receiveShadow ref={ref} object={flow.object3D} />
+  useFrame(() => flow.moveAlongCurve(0.001))
+
+  return (
+    <>
+      <primitive object={line} />
+      <primitive castShadow receiveShadow ref={ref} object={flow.object3D} />
+    </>
+  )
 }
 
 export default Sketch
